426aaf2d155cc9396bda6d6507afe998f5704ae3
Harutyun Mardirossian <harutyun.mardirossian@gmail.com>
2025-01-08 16:07:36 +0400

    Task02 - AWS hello world lambda with endpoints

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -3,7 +3,8 @@ package com.task02;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.google.gson.Gson;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
@@ -25,15 +26,15 @@ import java.util.Map;
         authType = AuthType.NONE,
         invokeMode = InvokeMode.BUFFERED
 )
-public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
     private static final Gson gson = new Gson();
 
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent request, Context context) {
         // Extract HTTP method and path from the request
-        String httpMethod = request.getHttpMethod();
-        String path = request.getPath();
+        String httpMethod = "GET";
+        String path = request.getRawPath();
 
         // Decide based on path and method
         if ("GET".equalsIgnoreCase(httpMethod) && "/hello".equalsIgnoreCase(path)) {
@@ -43,28 +44,28 @@ public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, A
         }
     }
 
-    private APIGatewayProxyResponseEvent handleGetHello() {
+    private APIGatewayV2HTTPResponse handleGetHello() {
         // Response body for success
         Map<String, Object> responseBody = new HashMap<>();
         responseBody.put("statusCode", 200);
         responseBody.put("message", "Hello from Lambda");
 
         // Setup the response
-        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
         response.setStatusCode(200);  // Explicitly for clarity
         response.setBody(gson.toJson(responseBody));  // Converts the Map to a JSON string
 
         return response;
     }
 
-    private APIGatewayProxyResponseEvent generateBadRequestResponse(String method, String path) {
+    private APIGatewayV2HTTPResponse generateBadRequestResponse(String method, String path) {
         // Response body for error
         Map<String, Object> responseBody = new HashMap<>();
         responseBody.put("statusCode", 400);
         responseBody.put("message", "Bad request syntax or unsupported method. Request path: " + path + ". HTTP method: " + method);
 
         // Setup a 400 Bad Request response
-        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
         response.setStatusCode(400);
         response.setBody(gson.toJson(responseBody));
 
